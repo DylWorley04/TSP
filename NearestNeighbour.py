f216fde06dc3c71054c47dae90e22b430604f3ca
DylWorley04 <D.Worley@2022.ljmu.ac.uk>
2025-02-27 13:43:42 +0000

    removed unnecessary code

--- NearestNeighbour.py
+++ NearestNeighbour.py
@@ -9,31 +9,31 @@ def load_tsp_file(filename): #function to define file name
 
 def tsp_nearest_neighbour(graph, cities): #defines function for nearest neighbour algorithm
     num_cities = len(cities) #gets the number of cities from the tsp file and then stores it in variable
-    start_city = cities[0] #sets the start city at start of problem
-    unvisited_cities = set(cities) #number of univistited cities 
-    unvisited_cities.remove(start_city) #removes the start city from the list of unvisited cities
+    start_city = cities[0] 
+    unvisited_cities = set(cities)  
+    unvisited_cities.remove(start_city) 
     
     current_city = start_city #starts at start city
     path = [start_city] 
-    total_cost = 0 #defines total cost and set to 0
+    total_cost = 0 
 
     while unvisited_cities: #while there are unvisited cities
         nearest_city = min(unvisited_cities, key=lambda city: graph[current_city][city]) #creates a temporary variable called lambda and sets it to the nearest city
-        total_cost += graph[current_city][nearest_city] #adds the cost of the nearest city to the total cost
-        path.append(nearest_city) #amends the path to include the nearest city
-        unvisited_cities.remove(nearest_city) #removes the nearest city from the list of unvisited cities
-        current_city = nearest_city #sets the current city to the previous nerest city
+        total_cost += graph[current_city][nearest_city] 
+        path.append(nearest_city) 
+        unvisited_cities.remove(nearest_city) 
+        current_city = nearest_city 
 
     # Return to the starting city
-    total_cost += graph[current_city][start_city] #adds the current city to the total cost
+    total_cost += graph[current_city][start_city] 
     path.append(start_city)
 
     return path, total_cost
 
 if __name__ == "__main__": #main function
-    filename = "./tsplib-master/berlin52.tsp" #load a tsp file using local file path
-    cities, graph = load_tsp_file(filename) #stores current file path in filename variable
+    filename = "./tsplib-master/berlin52.tsp" 
+    cities, graph = load_tsp_file(filename) 
 
-    best_path, min_cost = tsp_nearest_neighbour(graph, cities) #assigns the best path and minimum cost to the graph and citites values
-    print("Best path: ", best_path) #prints best path
-    print("Minimum cost: ", min_cost)#prints minimum cost
+    best_path, min_cost = tsp_nearest_neighbour(graph, cities) 
+    print("Best path: ", best_path) 
+    print("Minimum cost: ", min_cost)
